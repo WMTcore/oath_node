f63a19412c80b4aa2e6b9249639731bbf87a8164
WMTcore <291458560@qq.com>
2016-05-10 11:07:10 +0800

    Add get /qqoath

--- test/routes/index.js
+++ test/routes/index.js
@@ -3,18 +3,22 @@
 let express = require('express');
 let router = express.Router();
 let qqoath = require('../../lib');
+let _ = require('lodash');
+
+let redirect_uri = '';
 
 router.get('/', function(req, res, next) {
-	// console.error(qqoath)
 	let url = qqoath.GetAuthorizatioCode({
-		state: 'test'
+		state: qqoath.state(),
+		redirect_uri: redirect_uri
 	});
 	res.json(url);
 });
 
 router.get('/getaccesstoken', function(req, res, next) {
 	qqoath.GetAccessToken({
-		code: ''
+		code: '',
+		redirect_uri: redirect_uri
 	}).then((result) => res.json(result), (error) => res.status(500).json(error))
 })
 
@@ -22,6 +26,32 @@ router.get('/getopenid', function(req, res, next) {
 	qqoath.GetOpenID({
 		access_token: ''
 	}).then((result) => res.json(result), (error) => res.status(500).json(error))
+});
+
+router.get('/qqoath', function(req, res, next) {
+	let e = req.query,
+		result = {};
+	if (e.usercancel || e.state != qqoath.state() || !e.code)
+		return res.status(400).json(0);
+	qqoath.GetAccessToken({
+		code: e.code,
+		redirect_uri: redirect_uri
+	}).then((tokens) => {
+		result = tokens;
+		return qqoath.GetOpenID({
+			access_token: tokens.access_token
+		})
+	}, (error) => {
+		throw error;
+	}).then((openid) => {
+		_.merge(result, openid);
+		return res.json(result);
+	}, (error) => {
+		throw error;
+	}).catch((error) => {
+		return res.status(500).json(error)
+	})
 })
+
 
 module.exports = router;
